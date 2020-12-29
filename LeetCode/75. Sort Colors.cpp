54f7aec61eea5a0c50f1e9c723c1943a3485e3eb
romit0226 <67177041+romit0226@users.noreply.github.com>
2020-12-29 10:18:39 +0530

    Create 75. Sort Colors.cpp

---
+++ LeetCode/75. Sort Colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+       
+        int lo = 0, hi = nums.size()-1;
+        for (int i = 0; i <= hi; i++) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[lo++]);
+            } else if (nums[i] == 2) {
+                swap(nums[i--], nums[hi--]);
+            }
+        
+        }
+      
+    }
+};
